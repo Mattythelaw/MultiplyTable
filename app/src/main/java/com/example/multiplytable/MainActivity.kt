6e8a5d9aa0ede537010de4025c06b81164354212
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-04-01 16:25:30 +0200

    Added comments + val numinput

--- app/src/main/java/com/example/multiplytable/MainActivity.kt
+++ app/src/main/java/com/example/multiplytable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiplytable
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,9 +15,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        //Created values for to UI features//
+        //Added click button listener to lister for clicks//
         val multiplyBtn = findViewById<Button>(R.id.multiplyBtn)
+        val numInput = findViewById<EditText>(R.id.numTxtInput)
         multiplyBtn.setOnClickListener {
-            //create the explicit content//
+            //Added intent to switch pages//
             val intent = Intent(this, TableDisplay::class.java)
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
